941dc1b053e9233708e54ad5c71e1d867d062fb3
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-06-06 22:31:42 +0530

    Update Regression.R

--- Regression.R
+++ Regression.R
@@ -18,3 +18,23 @@ Regr
 
 plot(Y,X,main="Y data vs. X data",xlab="Y data",ylab="X data",col="red")
 abline(lm(X~Y))
+
+
+#Multiple Regression
+y=c(110,80,70,120,150,90,70,120)
+y
+x1=c(30,40,20,50,60,40,20,60)
+x1
+x2=c(11,10,7,15,19,12,8,14)
+x2
+
+#Linear Regression model for Y on X! and X2
+Reg_Model = lm(y~x1+x2)
+Reg_Model
+
+#Install.packages("scatterplot3d")
+library(scatterplot3d)
+
+#Plot the data set
+graph = scatterplot3d(x1,x2,y)
+graph$plane3d(Reg_Model)
